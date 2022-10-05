4e0d010ef60f767c8e3650d805979dbcff2fc4c4
GustavoMont <lgmluisgm@gmail.com>
2022-10-05 00:05:06 -0300

    :pencil: create dev setup

--- Data/Context.cs
+++ Data/Context.cs
@@ -5,17 +5,14 @@ namespace api_project.Data;
 
 public class Context : DbContext
 {
-    public Context(DbContextOptions<Context> options) : base(options)
-    {
+    public Context(DbContextOptions<Context> options) : base(options) { }
 
-    }
     //Tables
     public DbSet<Client> Clients { get; set; }
-    public DbSet<ClientContact> ClientContacts { get; set; } 
-    public DbSet<Service> Services { get; set; } 
+    public DbSet<ClientContact> ClientContacts { get; set; }
+    public DbSet<Service> Services { get; set; }
     public DbSet<ServiceType> ServiceTypes { get; set; }
     public DbSet<Firm> Firms { get; set; }
     public DbSet<Contract> Contracts { get; set; }
     public DbSet<Professional> Professionals { get; set; }
-
 }
