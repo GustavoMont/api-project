9c9b15879ccd5df4119824761a231c5790178e8e
GustavoMont <lgmluisgm@gmail.com>
2022-10-11 08:37:09 -0300

    :pencil: start crud in client model

--- Models/ClientModel.cs
+++ Models/ClientModel.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_project.Models;
 
+[Index(nameof(Email), IsUnique = true)]
 public class Client
 {
     [Required]
--- Services/ClientServices.cs
+++ Services/ClientServices.cs
@@ -1,5 +1,6 @@
 using api_project.Models;
 using api_project.Repositories;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using order_manager.Dto.Client;
 using System.Security.Cryptography;
@@ -15,14 +16,56 @@ public class ClientServices
         _repository = repository;
     }
 
-    public CreateClientReq CreateClient(CreateClientReq clientReq)
+    public GetClientRes CreateClient(CreateClientReq clientReq)
     {
         var client = new Client();
+        GetClientRes clientResponse = new();
         client.Name = clientReq.Name;
         client.Email = clientReq.Email;
         client.Password = BCrypt.Net.BCrypt.HashPassword(clientReq.Password);
         _repository.CreateClient(client);
-        clientReq.Password = "";
-        return clientReq;
+        client.Adapt(clientResponse);
+        return clientResponse;
+    }
+
+    public List<GetClientRes> GetAllClients()
+    {
+        var clients = _repository.GetAllClients();
+        List<GetClientRes> clientsResponse = new();
+        return clients.Adapt(clientsResponse);
+    }
+
+    public GetClientRes GetOneClient(int id)
+    {
+        var client = _repository.GetOneClient(id);
+        if (client is null)
+        {
+            return null;
+        }
+        GetClientRes clientResponse = new();
+        return client.Adapt(clientResponse);
+    }
+
+    public GetClientRes UpdateClient(int id, CreateClientReq updates)
+    {
+        var client = _repository.GetOneClient(id);
+        if (client is null)
+        {
+            return null;
+        }
+        var clientUpdate = updates.Adapt(client);
+        GetClientRes clientRes = new();
+        return clientUpdate.Adapt(clientRes);
+    }
+
+    public void DeleteClient(int id)
+    {
+        var client = _repository.GetOneClient(id);
+        if (client is null)
+        {
+            return;
+        }
+        _repository.DeleteClient(client);
+        return;
     }
 }
